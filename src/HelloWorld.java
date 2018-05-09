f028c8b1263af549729ed383209644e8cf84d536
Kai Chen <kaichenusesthis@gmail.com>
2018-05-09 16:50:02 -0700

    First commit.

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+import fish.fish;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String testVariable = "hello";
+        System.out.println("Hello world from Intellij");
+        fish.ugh();
+    }
+}
---
+++ src/fish/fish.java
@@ -0,0 +1,11 @@
+package fish;
+
+public class fish {
+    public static void main(String[] args) {
+
+    }
+
+    public static void ugh() {
+        System.out.println("Hey man");
+    }
+}
