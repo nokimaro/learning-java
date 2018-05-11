e5d00746bc13b1b9c4cb062fb3584d743ec95403
Kai Chen <kaichen120@gmail.com>
2018-05-11 14:59:39 -0700

    Dice roll counter.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,18 @@
+import java.util.Random;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        int john[] = {1, 2, 3, 4, 5};
-        int sum = 0;
+      Random rand = new Random();
+      int freq[] = new int[7];
 
-        for(int i : john) {
-            sum += i;
-        }
+      for(int roll = 1; roll < 1000; roll++) {
+          freq[1 + rand.nextInt(6)]++;
+      }
 
-        System.out.println("The sum of these numbers is: " + sum);
+      System.out.println("Face\tFrequency");
+
+      for(int face = 1; face<freq.length; face++) {
+          System.out.println(face + "\t" + freq[face]);
+      }
     }
 }
