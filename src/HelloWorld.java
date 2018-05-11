d2cad598b8c2b5146d564b95c57d4e5f51439678
Kai Chen <kaichen120@gmail.com>
2018-05-11 16:52:42 -0700

    Classes with multiple constructors and getter/setter methods.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        tuna tunaObject = new tuna();
-        System.out.println(tunaObject.toMilitary());
-        tunaObject.setTime(13, 27, 6);
-        System.out.println(tunaObject.toMilitary());
+        tuna tunaObject1 = new tuna();
+        tuna tunaObject2 = new tuna(5);
+        tuna tunaObject3 = new tuna(5, 13);
+        tuna tunaObject4 = new tuna(5, 13, 42);
 
+        System.out.printf("%s\n", tunaObject1.toMilitaryTime());
+        System.out.printf("%s\n", tunaObject2.toMilitaryTime());
+        System.out.printf("%s\n", tunaObject3.toMilitaryTime());
+        System.out.printf("%s\n", tunaObject4.toMilitaryTime());
       }
     }
--- src/tuna.java
+++ src/tuna.java
@@ -3,13 +3,56 @@ public class tuna {
     private int minute;
     private int second;
 
-    public void setTime(int h, int  m, int s) {
-        hour = ((h>=0 && h<24) ? h : 0);
-        minute = ((h>=0 && h<60) ? m : 0);
-        second = ((h>=0 && h<60) ? s : 0);
+    public tuna() {
+        this(0,0,0);
     }
 
-    public String toMilitary() {
-        return String.format("%02d:%02d:%02d", this.hour, this.minute, this.second);
+    public tuna(int h){
+        this(h, 0, 0);
     }
+
+    public tuna(int h, int m) {
+
+        this(h, m, 0);
+    }
+
+    public tuna(int h, int m, int s) {
+        setTime(h, m, s);
+    }
+
+    public void setTime(int h, int m, int s) {
+        setHour(h);
+        setMinute(m);
+        setSecond(s);
+    }
+
+    public void setHour(int h) {
+        hour = ((h >=0 && h < 27) ? h : 0);
+    }
+
+    public void setMinute(int m) {
+        minute = ((m >=0 && m < 60) ? m : 0);
+    }
+
+    public void setSecond(int s) {
+        second = ((s >=0 && s < 60) ? s : 0);
+    }
+
+    public int getHour() {
+        return this.hour;
+    }
+
+    public int getMinute() {
+        return this.minute;
+    }
+
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public String toMilitaryTime() {
+        return String.format("%02d:%02d:%02d", getHour(), getMinute(), getSecond());
+    }
+
 }
