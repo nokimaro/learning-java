d2f193430a61b519f858985f92ea698499b9cc6d
Kai Chen <kaichen120@gmail.com>
2018-05-11 15:03:06 -0700

    Multidimensional arrays.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,7 @@
-import java.util.Random;
-
 public class HelloWorld {
     public static void main(String[] args) {
-      Random rand = new Random();
-      int freq[] = new int[7];
+        int firstArray[][] = {{1,2,3,4,5,6,7}, {8,9,10,11,12,13}};
 
-      for(int roll = 1; roll < 1000; roll++) {
-          freq[1 + rand.nextInt(6)]++;
-      }
-
-      System.out.println("Face\tFrequency");
-
-      for(int face = 1; face<freq.length; face++) {
-          System.out.println(face + "\t" + freq[face]);
+        System.out.println(firstArray[0][1]);
       }
     }
-}
